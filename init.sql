8db1b9990d1552f040e998353a3e0b1106091f8e
Bikorimana Patrick <patrick.bikorimana@student.vinci.be>
2021-05-11 14:31:05 +0200

    commit: changed init.sql

--- init.sql
+++ init.sql
@@ -5,7 +5,7 @@ CREATE TABLE pae_project.adresses(
     id_adresse serial PRIMARY KEY,
     rue varchar(100),
     numero int,
-    boite varchar(10),	
+    boite varchar(20),	
     code_postal int,
     commune varchar(55),
     pays varchar(55)
@@ -21,7 +21,7 @@ CREATE TABLE pae_project.utilisateurs(
     prenom varchar(55),
     email varchar(100),
     date_inscription timestamp,
-    role varchar(10),
+    role varchar(20),
     adresse integer REFERENCES pae_project.adresses(id_adresse)
 );
 
@@ -37,7 +37,7 @@ CREATE TABLE pae_project.visites(
     plage_horaire varchar(255) NOT NULL,
     date_demande timestamp NOT NULL,
     date_visite timestamp,
-    etat_visite varchar(10)  NOT NULL,
+    etat_visite varchar(20)  NOT NULL,
     adresse integer references pae_project.adresses(id_adresse),
     explication TEXT
 );
@@ -45,7 +45,7 @@ CREATE TABLE pae_project.visites(
 
 CREATE TABLE pae_project.meubles(
     id_meuble serial PRIMARY KEY,
-    etat_meuble varchar(10)  NOT NULL,
+    etat_meuble varchar(20)  NOT NULL,
     type_meuble integer references pae_project.types_meubles(id_type) NOT NULL,
     description TEXT NOT NULL,
     prix_achat decimal,
@@ -69,7 +69,7 @@ CREATE TABLE pae_project.ventes(
     id_vente serial PRIMARY KEY,
     meuble integer references pae_project.meubles(id_meuble) NOT NULL,
     client integer references pae_project.utilisateurs(id_utilisateur),
-    etat_vente varchar(10)  NOT NULL,
+    etat_vente varchar(20)  NOT NULL,
     date_vente timestamp NOT NULL,
     date_emporte timestamp,
     date_livraison timestamp
@@ -103,10 +103,10 @@ INSERT INTO pae_project.adresses VALUES
 	(DEFAULT,'Sante des artistes','18',null,4800,'Verviers','Belgique');
 
 INSERT INTO pae_project.adresses VALUES 
-	(DEFAULT,'Rue des Minières','45',"Ter",4800,'Verviers','Belgique');
+	(DEFAULT,'Rue des Minières','45','Ter',4800,'Verviers','Belgique');
 
 INSERT INTO pae_project.adresses VALUES 
-	(DEFAULT,'Rue Victor Bouillenne','9',"4C",4800,'Verviers','Belgique');
+	(DEFAULT,'Rue Victor Bouillenne','9','4C',4800,'Verviers','Belgique');
 
 
 INSERT INTO pae_project.utilisateurs VALUES 
@@ -234,6 +234,47 @@ INSERT INTO pae_project.types_meubles VALUES --25
 
 
 
+
+
+
+
+
+
+INSERT INTO pae_project.meubles VALUES --1
+	(DEFAULT,'depose',1,'Bahut profond d une largeur de 112cm et d une hauteur de 147cm',200,null,null,null,1,'2021-03-30',null);
+
+INSERT INTO pae_project.meubles VALUES --2
+	(DEFAULT,'achete',2,'Large bureau 1m87 cm, deux colonnes de tirois',159,299,null,3,1,'2021-03-30',null);
+
+INSERT INTO pae_project.meubles VALUES --3
+	(DEFAULT,'annule',3,'Table jardin en bois brut',null,null,null,null,2,null,null);
+
+INSERT INTO pae_project.meubles VALUES --4
+	(DEFAULT,'retire',3,'Table en chaine, pieds en fer forge',140,459,null,null,3,'2021-03-29',null);
+
+INSERT INTO pae_project.meubles VALUES --5
+	(DEFAULT,'depose',4,'Secretaire en acajou, en marqueterie ',90,null,null,null,3,'2021-03-29',null);
+
+INSERT INTO pae_project.meubles VALUES --6
+	(DEFAULT,'propose',20,'Lit a baldaquin en acajou',null,null,null,null,4,null,null);
+
+INSERT INTO pae_project.meubles VALUES --7
+	(DEFAULT,'restauration',2,'Bureau en bois cire ',220,null,null,null,5,'2021-04-27',null);
+
+INSERT INTO pae_project.meubles VALUES --8
+	(DEFAULT,'vente',2,'Bureau en chaine massif, sous-main integre ',325,378,null,11,5,'2021-04-27',null);
+
+INSERT INTO pae_project.meubles VALUES --9
+	(DEFAULT,'vente',2,'Magnifique bureau en acajou ',180,239,null,null,5,'2021-04-27',null);
+
+INSERT INTO pae_project.meubles VALUES --10
+	(DEFAULT,'vente',12,'Splendide coiffeuse aux reliefs travailles ',150,199,null,15,5,'2021-04-27',null);
+
+INSERT INTO pae_project.meubles VALUES --11
+	(DEFAULT,'vente',12,'Coiffeuse marqueterie',145,199,null,17,5,'2021-04-27',null);
+
+
+
 INSERT INTO pae_project.photos_meubles VALUES --1
     (DEFAULT,1,'Bahut_2.jpg',false);
 
@@ -286,41 +327,6 @@ INSERT INTO pae_project.photos_meubles VALUES --17
     (DEFAULT,11,'Coiffeuse_2-Visible-Préférée.jpg',false);
 
 
-
-
-
-INSERT INTO pae_project.meubles VALUES --1
-	(DEFAULT,'depose',1,'Bahut profond d une largeur de 112cm et d une hauteur de 147cm',200,null,null,null,1,'2021-03-30',null);
-
-INSERT INTO pae_project.meubles VALUES --2
-	(DEFAULT,'achete',2,'Large bureau 1m87 cm, deux colonnes de tirois',159,299,null,3,1,'2021-03-30',null);
-
-INSERT INTO pae_project.meubles VALUES --3
-	(DEFAULT,'annule',3,'Table jardin en bois brut',null,null,null,null,2,null,null);
-
-INSERT INTO pae_project.meubles VALUES --4
-	(DEFAULT,'retire',3,'Table en chaine, pieds en fer forge',140,459,null,null,3,'2021-03-29',null);
-
-INSERT INTO pae_project.meubles VALUES --5
-	(DEFAULT,'depose',4,'Secretaire en acajou, en marqueterie ',90,null,null,null,3,'2021-03-29',null);
-
-INSERT INTO pae_project.meubles VALUES --6
-	(DEFAULT,'propose',20,'Lit a baldaquin en acajou',null,null,null,null,4,null,null);
-
-INSERT INTO pae_project.meubles VALUES --7
-	(DEFAULT,'restauration',2,'Bureau en bois cire ',220,null,null,null,5,'2021-04-27',null);
-
-INSERT INTO pae_project.meubles VALUES --8
-	(DEFAULT,'vente',2,'Bureau en chaine massif, sous-main integre ',325,378,null,11,5,'2021-04-27',null);
-
-INSERT INTO pae_project.meubles VALUES --9
-	(DEFAULT,'vente',2,'Magnifique bureau en acajou ',180,239,null,null,5,'2021-04-27',null);
-
-INSERT INTO pae_project.meubles VALUES --10
-	(DEFAULT,'vente',12,'Splendide coiffeuse aux reliefs travailles ',150,199,null,15,5,'2021-04-27',null);
-
-INSERT INTO pae_project.meubles VALUES --11
-	(DEFAULT,'vente',12,'Coiffeuse marqueterie',145,199,null,17,5,'2021-04-27',null);
 
 
 
